81bc1051ec8691db7cad1e6ecc7883e267982015
Yaroslav <75562001+losereasp@users.noreply.github.com>
2021-09-23 14:47:08 +0300

    Delete 2 lab directory

--- 2 lab/2.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main() {
-	int a, b, c;//Объявление перменных
-	cin >> a >> b >> c;//Ввод точек на оси
-	cout << "AC = " << abs(c - a) << endl << "BC = " << abs(c - b) << endl << "SUM = " << abs(c - a) + abs(c - b);//Вывод длины отрезков АС и ВС
-}
--- 2 lab/3.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main() {
-	int a, b, c;//Объявление переменных
-	cin >> a >> b >> c;//Ввод точек
-	cout << abs(c - a) * abs(c - b);//Вывод произведения ВС и АС
-}
