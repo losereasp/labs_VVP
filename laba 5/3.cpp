ac39f24b17123ac277cb0333111fdfc876d9b4db
Yaroslav <75562001+losereasp@users.noreply.github.com>
2021-09-29 00:47:50 +0300

    Add files via upload

---
+++ laba 5/3.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b;
+	cout << "enter segment a and segment b (a > b):" << endl;
+	cin >> a >> b;
+	cout << "length of the unoccupied part of the segment: " << endl;
+	cout << a % b;
+}
