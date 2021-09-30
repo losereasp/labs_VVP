1d59cabd5e8476836aefad26b9718804684cf3b8
Yaroslav <75562001+losereasp@users.noreply.github.com>
2021-09-30 22:23:37 +0300

    Add files via upload

---
+++ laba 5/5.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a;//объявляем переменную 
+	cout << "enter three-digit number:" << endl;
+	cin >> a;//вводим трехзначное число
+	cout << "the first digit is ahead of this number:" << endl;
+	cout << a / 10 % 10 << a % 10 << a / 100;//выводим первую цифру числа в конце
+}
