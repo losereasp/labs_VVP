6bb377c81e107693d34a694edb6185c7ed21de5d
Yaroslav <75562001+losereasp@users.noreply.github.com>
2021-09-30 22:26:21 +0300

    Update 2.cpp

--- laba 6/2.cpp
+++ laba 6/2.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 
 using namespace std;
 
@@ -7,5 +7,26 @@ int main() {
 	cout << "enter number of the day:" << endl;
 	cin >> k;//Вводим день года
 	cout << "number of the day of week:" << endl;
-	cout << k % 7;//Выводим день недели
+	k %= 7;
+	if (k == 0) {
+		cout << k <<" - sunday";
+	}
+	if (k == 1) {
+		cout << k << " - monday";
+	}
+	if (k == 2) {
+		cout << k << " - tuesday";
+	}
+	if (k == 3) {
+		cout << k << " - wednesday";
+	}
+	if (k == 4) {
+		cout << k << " - thursday";
+	}
+	if (k == 5) {
+		cout << k << " - friday";
+	}
+	if (k == 6) {
+		cout << k << " - saturday";
+	}
 }
