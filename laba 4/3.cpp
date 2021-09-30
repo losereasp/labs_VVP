3ca62e1de0686958cc2bc0a4646db062cba92221
Yaroslav <75562001+losereasp@users.noreply.github.com>
2021-09-30 22:19:05 +0300

    Add files via upload

---
+++ laba 4/3.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int x, a, y;//Объявляем переменные
+	cout << "enter kg of sweets and their cost:" << endl;
+	cin >> x >> a;//Вводим х кг а рублей и у кг
+	cout << "1 kg price: " << a / x << endl;//Выводим стоимость 1 кг
+	cout << "enter the weight of the same candies" << endl;
+	cin >> y;
+	cout << y << " kg price: " << y * (a / x);//Выводим стоимость у кг
+}
