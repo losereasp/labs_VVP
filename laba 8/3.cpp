115025f751be6c02e4d116b649bd6803ffdeef85
Yaroslav <75562001+losereasp@users.noreply.github.com>
2021-10-07 15:14:06 +0300

    Update 3.cpp

--- laba 8/3.cpp
+++ laba 8/3.cpp
@@ -1,22 +1,23 @@
-﻿#include <iostream>
+#include <iostream>
 #include <cmath>
 using namespace std;
 
-int distance(int x1, int y1, int x2, int y2) {
+float distance(float x1, float y1, float x2, float y2) {
 	return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));//Функция просчета расстояния между точками
 }
 
 int main() {
-	int x1, y1, x2, y2, x3, y3;//Объявляем переменные
+	float x1, y1, x2, y2, x3, y3;//Объявляем переменные
 	cout << "enter coordinates x and y point A:" << endl;
 	cin >> x1 >> y1;//Вводим координаты А
 	cout << "enter coordinates x and y point B:" << endl;
 	cin >> x2 >> y2;//Вводим координаты Б
 	cout << "enter coordinates x and y point C:" << endl;
 	cin >> x3 >> y3;//Вводим координаты С
-	int ab = distance(x1, y1, x2, y2);//Расстояние А - В
-	int ac = distance(x1, y1, x3, y3);//Расстояние А - С
+	float ab = distance(x1, y1, x2, y2);//Расстояние А - В
+	float ac = distance(x1, y1, x3, y3);//Расстояние А - С
 	if (ab > ac) cout << "point: c " << endl;
-	else cout << "point: b " << endl;
-	cout << "distance: " << min(ab, ac);
+	else cout << "point: b " << endl; 
+	cout << "distance: " << min(ab,ac) << endl;
+	cout << ab;
 }
