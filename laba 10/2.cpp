06b490becb3bba8d819a0c937979ab5412cf44e2
Yaroslav <75562001+losereasp@users.noreply.github.com>
2021-10-18 22:18:10 +0300

    Update 2.cpp

--- laba 10/2.cpp
+++ laba 10/2.cpp
@@ -1,18 +1,18 @@
-//#include <iostream>
-//
-//using namespace std;
-//
-//int main() {
-//	setlocale(LC_ALL, "rus");
-//	float a = 1.1, res = 1;
-//	int i = 1, n;
-//	cout << "Ââåäèòå öåëîå ÷èñëî > 0:" << endl;
-//	cin >> n;
-//	while (i <= n) {
-//		res *= a;
-//		a += 0.1;
-//		i++;
-//	}
-//	cout << "Ðåçóëüòàò óìíîæåíèÿ 1.1 * 1.2 * 1.3 * .... (" << n << " ñîìíîæèòåëåé):" << endl;
-//	cout << res;
-//}
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "rus");
+	float a = 1.1, res = 1;
+	int i = 1, n;
+	cout << "Ã‚Ã¢Ã¥Ã¤Ã¨Ã²Ã¥ Ã¶Ã¥Ã«Ã®Ã¥ Ã·Ã¨Ã±Ã«Ã® > 0:" << endl;
+	cin >> n;
+	while (i <= n) {
+		res *= a;
+		a += 0.1;
+		i++;
+	}
+	cout << "ÃÃ¥Ã§Ã³Ã«Ã¼Ã²Ã Ã² Ã³Ã¬Ã­Ã®Ã¦Ã¥Ã­Ã¨Ã¿ 1.1 * 1.2 * 1.3 * .... (" << n << " Ã±Ã®Ã¬Ã­Ã®Ã¦Ã¨Ã²Ã¥Ã«Ã¥Ã©):" << endl;
+	cout << res;
+}
