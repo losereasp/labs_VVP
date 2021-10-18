272153bace9fba9421ccde7f667d9aa7714257e7
Yaroslav <75562001+losereasp@users.noreply.github.com>
2021-10-18 22:18:43 +0300

    Update 3.cpp

--- laba 10/3.cpp
+++ laba 10/3.cpp
@@ -1,15 +1,15 @@
-//#include <iostream>
-//
-//using namespace std;
-//
-//int main() {
-//	setlocale(LC_ALL, "rus");
-//	int n, a = 0;
-//	cout << "Ââåäèòå öåëîå ÷èñëî > 0:" << endl;
-//	cin >> n;
-//	for (int i = 1; i <= 2 * n - 1; i+=2) {
-//		a += i;
-//		cout << a << " ";
-//	}
-//	cout << endl << n << " â êâàäðàòå = " << a;
-//}
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "rus");
+	int n, a = 0;
+	cout << "Ã‚Ã¢Ã¥Ã¤Ã¨Ã²Ã¥ Ã¶Ã¥Ã«Ã®Ã¥ Ã·Ã¨Ã±Ã«Ã® > 0:" << endl;
+	cin >> n;
+	for (int i = 1; i <= 2 * n - 1; i+=2) {
+		a += i;
+		cout << a << " ";
+	}
+	cout << endl << n << " Ã¢ ÃªÃ¢Ã Ã¤Ã°Ã Ã²Ã¥ = " << a;
+}
