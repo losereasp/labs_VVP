525e70b55eb6a355283324459035300b0f637ecd
Yaroslav <75562001+losereasp@users.noreply.github.com>
2021-10-24 13:56:08 +0300

    Update 5.cpp

--- laba 13/5.cpp
+++ laba 13/5.cpp
@@ -1,25 +1,25 @@
-//#include <iostream>
-//#include <vector>
-//
-//using namespace std;
-//
-//int main() {
-//	setlocale(LC_ALL, "rus");
-//	int n;
-//	vector <int> a;
-//	cout << "Введите размер массива N:" << endl;
-//	cin >> n;
-//	for (int i = 1; i <= n; i++) {
-//		a.push_back(i);//Числа в вектор вводиим
-//	}
-//	cout << "Номера под нечетными индексами:" << endl;
-//	for (int i = 0; i < a.size(); i++) {
-//		if(i % 2 != 0)//Проверяем на нечетность
-//		cout << a[i] << " ";//Выводим
-//	}
-//	cout << endl << "Номера под четными индексами:" << endl;
-//	for (int i = a.size() - 1; i >= 0; i--) {
-//		if(i % 2 == 0)//Проверяем на четность
-//		cout << a[i] << " ";//Выводим
-//	}
-//}
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "rus");
+	int n;
+	vector <int> a;
+	cout << "Р’РІРµРґРёС‚Рµ СЂР°Р·РјРµСЂ РјР°СЃСЃРёРІР° N:" << endl;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		a.push_back(i);//Р§РёСЃР»Р° РІ РІРµРєС‚РѕСЂ РІРІРѕРґРёРёРј
+	}
+	cout << "РќРѕРјРµСЂР° РїРѕРґ РЅРµС‡РµС‚РЅС‹РјРё РёРЅРґРµРєСЃР°РјРё:" << endl;
+	for (int i = 0; i < a.size(); i++) {
+		if(i % 2 != 0)//РџСЂРѕРІРµСЂСЏРµРј РЅР° РЅРµС‡РµС‚РЅРѕСЃС‚СЊ
+		cout << a[i] << " ";//Р’С‹РІРѕРґРёРј
+	}
+	cout << endl << "РќРѕРјРµСЂР° РїРѕРґ С‡РµС‚РЅС‹РјРё РёРЅРґРµРєСЃР°РјРё:" << endl;
+	for (int i = a.size() - 1; i >= 0; i--) {
+		if(i % 2 == 0)//РџСЂРѕРІРµСЂСЏРµРј РЅР° С‡РµС‚РЅРѕСЃС‚СЊ
+		cout << a[i] << " ";//Р’С‹РІРѕРґРёРј
+	}
+}
